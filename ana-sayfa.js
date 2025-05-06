f95fed754993e2c6ed6d6b3c7b5c98c1147bf730
nisa yazoğlu <nisa.yazoglu@ogr.sakarya.edu.tr>
2025-05-06 23:46:20 +0300

    bazı temel değişikliklere gidilmeye karar verildi. O yüzden ana sayfadaki her şeyi siliyorum

--- ana-sayfa.js
+++
@@ -1,7 +0,0 @@
-let currentPage = 0;
-    const slider = document.getElementById('slider');
-
-    function goToPage(index) {
-      currentPage = index;
-      slider.style.transform = `translateX(-${index * 100}vw)`;
-    }
