bf1f85bc887d5384f20d329e9850cd86c006f989
nisa yazoğlu <nisa.yazoglu@ogr.sakarya.edu.tr>
2025-05-15 19:17:32 +0300

    memleket sayfasının ilk kısmı tamamlandı

---
+++ memleket.js
@@ -0,0 +1,16 @@
+
+
+  const sehirOgeleri = document.querySelectorAll('.sehir-item');
+  const bilgiMetni = document.getElementById('bilgiMetni');
+
+  sehirOgeleri.forEach(function (item) {
+    item.addEventListener('mouseover', function () {
+      const yeniYazi = item.getAttribute('data-info');
+      bilgiMetni.textContent = yeniYazi;
+    });
+
+    item.addEventListener('mouseout', function () {
+      bilgiMetni.textContent = "Bayburt, Doğu Karadeniz'de bulunan ve Çoruh Nehri'ne yaslanan Türkiye'nin bir şehri ve Bayburt ilinin merkezidir.";
+    });
+  });
+
